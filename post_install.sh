cc0959768bd483dff90c1c68f3c6d7ac8d13e596
Stefan Schramek <stefanschramek@users.noreply.github.com>
2020-04-03 18:04:42 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # Initialize defaults
-HOST_NAME="nextcloud.nomadic.io"
+HOST_NAME="nextcloud.domain.tld"
 DL_FLAGS="tls.dns.azure"
 DNS_SETTING="dns azure"
 TIME_ZONE="Europe/Vienna"
+JAIL_IP="%%IP%%"
+CERT_EMAIL="email@domain.tld"
+DNS_ENV="AZURE_CLIENT_ID=xxx AZURE_CLIENT_SECRET=xxx AZURE_SUBSCRIPTION_ID=xxx AZURE_TENANT_ID=xxx AZURE_RESOURCE_GROUP=xxx AZURE_METADATA_ENDPOINT=https://login.microsoftonline.com/"
 
-
-JAIL_IP=""
 DEFAULT_GW_IP=""
 INTERFACE="vnet0"
 VNET="on"
@@ -79,3 +80,48 @@ sed -i '' "s/yourhostnamehere/${HOST_NAME}/" /usr/local/www/Caddyfile
 sed -i '' "s/DNS-PLACEHOLDER/${DNS_SETTING}/" /usr/local/www/Caddyfile
 sed -i '' "s/JAIL-IP/${JAIL_IP}/" /usr/local/www/Caddyfile
 sed -i '' "s|mytimezone|${TIME_ZONE}|" /usr/local/etc/php.ini
+
+sysrc caddy_enable="YES"
+sysrc caddy_cert_email="${CERT_EMAIL}"
+sysrc caddy_env="${DNS_ENV}"
+
+# Secure database, set root password, create Nextcloud DB, user, and password
+mysql -u root -e "CREATE DATABASE nextcloud;"
+mysql -u root -e "GRANT ALL ON nextcloud.* TO nextcloud@localhost IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
+mysql -u root -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+mysql -u root -e "DROP DATABASE IF EXISTS test;"
+mysql -u root -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+mysql -u root -e "UPDATE mysql.user SET Password=PASSWORD('${DB_ROOT_PASSWORD}') WHERE User='root';"
+mysqladmin reload
+cp -f /mnt/includes/my.cnf /root/.my.cnf
+sed -i '' "s|mypassword|${DB_ROOT_PASSWORD}|" /root/.my.cnf
+
+# CLI installation and configuration of Nextcloud
+touch /var/log/nextcloud.log
+chown www /var/log/nextcloud.log
+su -m www -c "php /usr/local/www/nextcloud/occ maintenance:install --database=\"mysql\" --database-name=\"nextcloud\" --database-user=\"nextcloud\" --database-pass=\"${DB_PASSWORD}\" --database-host=\"localhost:/tmp/mysql.sock\" --admin-user=\"admin\" --admin-pass=\"${ADMIN_PASSWORD}\" --data-dir=\"/mnt/files\""
+su -m www -c "php /usr/local/www/nextcloud/occ config:system:set mysql.utf8mb4 --type boolean --value=\"true\""
+su -m www -c "php /usr/local/www/nextcloud/occ db:add-missing-indices"
+su -m www -c "php /usr/local/www/nextcloud/occ db:convert-filecache-bigint --no-interaction"
+su -m www -c "php /usr/local/www/nextcloud/occ config:system:set logtimezone --value=\"${TIME_ZONE}\""
+su -m www -c 'php /usr/local/www/nextcloud/occ config:system:set log_type --value="file"'
+su -m www -c 'php /usr/local/www/nextcloud/occ config:system:set logfile --value="/var/log/nextcloud.log"'
+su -m www -c 'php /usr/local/www/nextcloud/occ config:system:set loglevel --value="2"'
+su -m www -c 'php /usr/local/www/nextcloud/occ config:system:set logrotate_size --value="104847600"'
+su -m www -c 'php /usr/local/www/nextcloud/occ config:system:set memcache.local --value="\OC\Memcache\APCu"'
+su -m www -c 'php /usr/local/www/nextcloud/occ config:system:set redis host --value="/tmp/redis.sock"'
+su -m www -c 'php /usr/local/www/nextcloud/occ config:system:set redis port --value=0 --type=integer'
+su -m www -c 'php /usr/local/www/nextcloud/occ config:system:set memcache.locking --value="\OC\Memcache\Redis"'
+su -m www -c "php /usr/local/www/nextcloud/occ config:system:set overwrite.cli.url --value=\"https://${HOST_NAME}/\""
+su -m www -c 'php /usr/local/www/nextcloud/occ config:system:set htaccess.RewriteBase --value="/"'
+su -m www -c 'php /usr/local/www/nextcloud/occ maintenance:update:htaccess'
+su -m www -c "php /usr/local/www/nextcloud/occ config:system:set trusted_domains 1 --value=\"${HOST_NAME}\""
+su -m www -c "php /usr/local/www/nextcloud/occ config:system:set trusted_domains 2 --value=\"${JAIL_IP}\""
+su -m www -c 'php /usr/local/www/nextcloud/occ app:enable encryption'
+su -m www -c 'php /usr/local/www/nextcloud/occ encryption:enable'
+su -m www -c 'php /usr/local/www/nextcloud/occ encryption:disable'
+su -m www -c 'php /usr/local/www/nextcloud/occ background:cron'
+su -m www -c 'php -f /usr/local/www/nextcloud/cron.php'
+crontab -u www /tmp/www-crontab
+
